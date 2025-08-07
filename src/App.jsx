d497149209b8fe1878a816b8440dc57ed2e1ba4e
Marie Claudine Ingenere <ingenerem@gmail.com>
2025-08-07 15:56:31 -0400

    refactor the App function

--- src/App.jsx
+++ src/App.jsx
@@ -5,27 +5,15 @@ import skillData from './data/skillData.json'
 
 function App() {
 
-  const skills = skillData;
+  //const skills = skillData;
+  const skills = skillData.map(skill=> <TechItem item={skill.logo}
+                 desc={skill.description} web={skill.link} name={skill.name}/>)
 
   return (
     <>
     <Header/>
     <section className='skill-container'>
-      {skills.map((skill, index) => (
-
-        
-               <TechItem
-                 key={index}        
-                 item={skill.logo}
-                 desc={skill.description}
-                 web={skill.link}
-                 name={skill.name}
-              />
-      ))}
-
-
-
-      
+      {skills}
     </section>
    
     </>
